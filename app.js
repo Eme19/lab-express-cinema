937f057824d2cb11f43aaa8184c9d242c99ee87f
“Emeka <ngwe7236@gmail.com>
2023-06-03 02:49:55 +0200

    done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
+
 const express = require('express');
 
 // Handles the handlebars
@@ -27,6 +28,8 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
+const mongoose = require('mongoose'); 
+const Movie = require('../models/Movie.model');
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-cinema";
+
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res, next) =>  {
+    mongoose.connect(MONGO_URI)
+    .then(()=> Movie.find())
+    .then(allcinemaMovie =>  {
+        console.log('retrieve all movies in DB:', allcinemaMovie)
+        res.render('movies', {movies: allcinemaMovie})
+    })
+    .then(()=> mongoose.connection.close())
+    .catch(error => console.log('erro getting information from db'))
+})
+
+router.get('/movie/:id', (req, res, next) =>  {
+    mongoose.connect(MONGO_URI)
+    .then(()=> Movie.findById(req.params.id))
+    .then(cinemaMovie =>  {
+        console.log('retrieve all movies in DB:', cinemaMovie)
+        res.render('movie', {movie: cinemaMovie})
+       
+    })
+    .then(()=> mongoose.connection.close())
+    .catch(error => console.log('erro getting information from db'))
+})
 module.exports = router;
+
+
+// const movie = require('./routes/movies');
+// app.use('/', movie)
